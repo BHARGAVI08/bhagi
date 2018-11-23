f24d1938142c22f7120bf6f5adca00a0ce3df25d
BHARGAVI08 <45282901+BHARGAVI08@users.noreply.github.com>
2018-11-23 14:05:07 +0530

    Create character.py

---
+++ character.py
@@ -0,0 +1,7 @@
+l=input("Input a letter of the alphabet:")
+if l in ('a','e','i','o','u'):
+  printf("%s is vowel." %l)
+elif l=='y':
+  printf("sometimes letter y stand for vowel,sometimes stand for consonant")
+else:
+  printf("%s is a consonant."%l)
