f6051a4479e893d790bd9b466ca57f0eb10e249c
BHARGAVI08 <45282901+BHARGAVI08@users.noreply.github.com>
2018-11-24 18:43:55 +0530

    Create multiple.py

---
+++ multiple.py
@@ -0,0 +1,12 @@
+def multiple(m, n): 
+  
+    # inserts all elements from n to  
+    # (m * n)+1 incremented by n. 
+    a = range(5, (m * 5)+1, 5) 
+      
+    print(*a) 
+  
+# driver code  
+m = 5
+n = 5
+multiple(m, 5) 
